a5fa637a9299713d7353aa70c0585d543e64a0dd
luung <luung@TRUNGTIN>
2023-08-08 13:58:26 +0700

    Add project files.

---
+++ e.ShopSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e.ShopSolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
