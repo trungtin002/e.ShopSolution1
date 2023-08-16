2098aace4c584fdd98816bff0b564cf1151cf5ef
luung <luunguyentrungtri@gmail.com>
2023-08-16 23:10:00 +0700

    Add api_login

--- e.ShopSolution.BackEndAPI/Controllers/UsersController.cs
+++ e.ShopSolution.BackEndAPI/Controllers/UsersController.cs
@@ -18,7 +18,7 @@ namespace e.ShopSolution.BackEndAPI.Controllers
 
         [HttpPost("authenticate")]
         [AllowAnonymous]
-        public async Task<IActionResult> Authenticate([FromForm] LoginRequest request)
+        public async Task<IActionResult> Authenticate([FromBody] LoginRequest request)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -33,7 +33,7 @@ namespace e.ShopSolution.BackEndAPI.Controllers
 
         [HttpPost("register")]
         [AllowAnonymous]
-        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
--- eShopSolution.AdminApp/Program.cs
+++ eShopSolution.AdminApp/Program.cs
@@ -1,17 +1,33 @@
+using eShopSolution.AdminApp.Services;
+using eShopSolution.ViewModels.System.Users;
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<IUserApiClient, UserApiClient>();
+
+
+builder.Services.AddControllers()
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginRequest>());
+var app = builder.Build();  
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
